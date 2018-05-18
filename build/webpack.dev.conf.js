3edbaf87bb33607e86d9599b208de4749abcfd42
dandan18cute <1125090339@qq.com>
2018-05-18 18:27:32 +0800

    resource

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -9,16 +9,14 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 const portfinder = require('portfinder')
-
-// var app = express();
-
-// var appData = require('../data.json');
-// var seller = appData.seller;
-// var goods = appData.goods;
-// var ratings = appData.ratings;
-
-// var apiRoutes = express.Router();
-
+const express = require('express')
+const app = express()// 请求server
+var appData = require('../data.json');// 加载本地数据文件
+var seller = appData.seller;
+var goods = appData.goods;
+var ratings = appData.ratings;
+var apiRoutes = express.Router();
+app.use('/api', apiRoutes)
 // apiRoutes.get('/seller',function (req,res) {
 //   res.json({
 //     errno:0,
@@ -38,7 +36,7 @@ const portfinder = require('portfinder')
 //     errno:0,
 //     data:ratings
 //   });
-//   // body...
+//   // body..
 // });
 
 // app.use('/api',apiRoutes);
@@ -76,6 +74,26 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     watchOptions: {
       poll: config.dev.poll,
     }
+  },
+  before(app) {
+    app.get('/api/seller', (req, res) => {
+      res.json({
+        errno: 0,
+        data,seller
+      })//接口返回json数据，上面配置的数据seller就赋值给data请求后调用
+    }),
+    app.get('/api/goods', (req, res) => {
+      res.json({
+        errno: 0,
+        data,goods
+      })，
+    }),
+    app.get('/api/ratings', (req, res) => {
+      res.json({
+        errno: 0,
+        data,ratings
+      })，
+    }),
   },
   plugins: [
     new webpack.DefinePlugin({
