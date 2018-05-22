32490e29fe3f7383db56167ab4ea7882bea08d87
dandan18cute <1125090339@qq.com>
2018-05-22 14:40:33 +0800

    star components

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -31,18 +31,8 @@
 <div v-show="detailShow" class="detail">
   <div class="detail-wrapper clearfix">
     <div class="detail-main">
-      <p>{{seller.bulletin}}</p>
-      <p>{{seller.bulletin}}</p>
-      <p>{{seller.bulletin}}</p>
-      <p>{{seller.bulletin}}</p>
-      <p>{{seller.bulletin}}</p>
-      <p>{{seller.bulletin}}</p>
-      <p>{{seller.bulletin}}</p>
-      <p>{{seller.bulletin}}</p>
-      <p>{{seller.bulletin}}</p>
-      <p>{{seller.bulletin}}</p>
-      <p>{{seller.bulletin}}</p>
-      <p>{{seller.bulletin}}</p>
+      <h1 class="name">{{seller.name}}</h1>
+      <star :size="48" :score="seller.score"></star>
     </div>
   </div>
   <div class="detail-close">
@@ -52,6 +42,7 @@
 </div>
 </template>
 <script type="text/javascript">
+import star from '../star/star.vue';
 export default {
   props: {
     seller: {
@@ -70,6 +61,9 @@ export default {
   },
   created () {
     this.classMap = ['decrease', 'discount', 'guarantee', 'invoice', 'special'];
+  },
+  components: {
+  	star
   }
 };
 </script>
@@ -195,10 +189,16 @@ export default {
     overflow: auto
     background: rgba(7,17,27,0.8)
     .detail-wrapper
+      width: 100%
       min-height: 100%
       .detail-main
         margin-top: 64px
         padding-bottom: 64px
+        .name
+          line-height: 16px
+          text-align: center
+          font-size: 16px
+          font-weight: 700
     .detail-close
       position: relative
       width: 32px
