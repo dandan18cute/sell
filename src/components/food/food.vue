e82d9ee8680dfa9939860f1ec2ce1033449e9862
dandan18cute <1125090339@qq.com>
2018-06-06 18:37:09 +0800

    food compent

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -1,10 +1,30 @@
 <template>
  <transition name="move">
- <div v-show="showFlag" class="food">
+ <div v-show="showFlag" class="food" ref="food">
+   <div class="food-content">
+   	<div class="image-header">
+   	  <img :src="food.image">
+   	  <div class="back" @click="hide">
+   	  	<i class="icon-arrow_lift"></i>
+   	  </div>
+   	<div class="content">
+   	  <h1 class="title">{{food.name}}</h1>
+   	  <div class="detail">
+   	  	<span class="sell-count">月售{{food.sellCount}}份</span>
+   	  	<span class="rating">好评率{{food.rating}}</span>
+   	  </div>
+   	  <div class="price">
+   	  	<span class="now">￥{{food.price}}</span>
+   	  	<span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+   	  </div>
+   	</div>
+   	</div>
+   </div>
  </div>
  </transition>
 </template>
 <script type="=text/ecmascript-6">
+  import BScroll from 'better-scroll';
 export default{
   props: {
     food: {
@@ -19,6 +39,18 @@ export default{
   methods: {
     show() {
       this.showFlag = true;
+      this.$nextTick(() => {
+        if(!this.scroll){
+          this.scroll = new BScroll(this.$ref.food,{
+            click: true
+          });
+        }else {
+          this.scroll.refresh();
+        }
+      });
+    },
+    hide() {
+      this.showFlag = false;
     }
   },
 };
@@ -37,7 +69,55 @@ export default{
       transition: all 0.2s linear
     &.move-enter,&.move-leave-active
       transform: translate3d(100%,0,0)
-
+    .image-header
+      position: relative
+      width: 100%
+      height: 0
+      padding-top: 100%
+      img
+        position: absolute
+        top: 0
+        left: 0
+        width: 100%
+        height: 100%
+      .back
+      	position: absolute
+      	top: 10px
+      	left: 0
+      	.icon-arrow_lift
+      	  display: block
+      	  padding: 10px
+      	  font-size: 20px
+      	  color: #fff
+    .content
+      padding: 18px
+      .title
+        line-heiht: 14px
+        margin-bottom: 8px
+        font-size: 14px
+        font-weight: 700
+        color: rgb(7,17,27)
+      .detail
+      	margin-bottom: 18px
+      	line-height: 10px
+      	height: 10px
+      	font-size: 0
+      	.sell-count,.rating
+      	  font-size: 10px
+      	  color: rgb(147,153,159)
+      	.sell-count
+      	  margin-right: 12px
+      .price
+      	font-weight: 700
+      	line-height: 24px
+      	.now
+      	  margin-right: 8px
+      	  font-size: 14px
+      	  color: rgb(240,20,20)
+      	.old
+      	  text-decoration:  line-through
+      	  font-size: 10px
+      	  color: rgb(147,153,159)
 
 
 </style>
