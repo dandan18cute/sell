52e4d8841a507f5c1bcf3cf234bcf50598b5d9ed
dandan18cute <1125090339@qq.com>
2018-06-19 11:08:04 +0800

    commodity introduce

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -18,13 +18,18 @@
    	  	<span class="now">￥{{food.price}}</span>
    	  	<span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
    	  </div>
-   	<div class="cartcontrol-wrapper">
-   	  <cartControl  :food="food"></cartControl>
-   	</div>
+      <div class="cartcontrol-wrapper">
+      <cartControl  :food="food"></cartControl>
+    </div>
     <transition name="fade">
-   	<div @click.stop.prevent="addFirst" class="buy" v-show="!food.count || food.count ===0">加入购物车</div>
+    <div @click.stop.prevent="addFirst" class="buy" v-show="!food.count || food.count ===0">加入购物车</div>
     </transition>
    	</div>
+    <splite v-show="food.info"></splite>
+    <div class="info" v-show="food.info">
+      <h1 class="title">商品信息</h1>
+      <p class="text">{{food.info}}</p>
+    </div>
    </div>
  </div>
  </transition>
@@ -33,6 +38,7 @@
   import BScroll from 'better-scroll';
   import Vue from 'vue';
   import cartControl from '../../components/cartControl/cartControl';
+  import splite from '../../components/splite/splite';
 export default{
 props: {
   food: {
@@ -87,6 +93,7 @@ methods: {
   },
   components: {
     cartControl,
+    splite
   },
 }
 </script>
@@ -126,6 +133,10 @@ methods: {
         font-size: 20px
         color: #fff
     .content
+      
+
+
+      position: relative
       padding: 18px
       .title
         line-height: 14px
@@ -154,27 +165,39 @@ methods: {
       	  text-decoration:  line-through
       	  font-size: 10px
       	  color: rgb(147,153,159)
-    .cartcontrol-wrapper
-      position: absolute
-      right: 12px
-      bottom: 12px
-    .buy
-      position: absolute
-      right: 18px
-      bottom: 18px
-      z-index: 10
-      height: 24px
-      line-height: 24px
-      padding: 0 12px
-      box-sizing: border-box
-      border-radius: 12px
-      font-size: 10px
-      color: #fff
-      background: rgb(0,160,220)
-      opacity: 1
-      &.fade-enter-active, &.fade-leave-active
-        transition: all 0.2s
-      &.fade-enter,&.fade-leave-active
-        opacity: 0
-        z-index: -1
+      .cartcontrol-wrapper
+        position: absolute
+        right: 12px
+        bottom: 12px
+      .buy
+        position: absolute
+        right: 18px
+        bottom: 18px
+        z-index: 10
+        height: 24px
+        line-height: 24px
+        padding: 0 12px
+        box-sizing: border-box
+        border-radius: 12px
+        font-size: 10px
+        color: #fff
+        background: rgb(0,160,220)
+        opacity: 1
+        &.fade-enter-active, &.fade-leave-active
+          transition: all 0.2s
+        &.fade-enter,&.fade-leave-active
+          opacity: 0
+          z-index: -1
+    .info
+      padding: 18px
+      .title
+        line-height: 14px
+        margin-bottom: 6px
+        font-size: 14px
+        color: rgb(7,17,27)
+      .text
+        line-height: 24px
+        padding: 0 8px
+        font-size: 12px
+        color: rgb(77,85,93)
 </style>
