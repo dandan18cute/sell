bb6dfeea3bd40f3b186bf8e120967c3dc44295b9
dandan18cute <1125090339@qq.com>
2018-06-22 18:31:36 +0800

    20180622

--- src/router/Seller/seller.vue
+++ src/router/Seller/seller.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="seller">
+  <div class="seller" ref="seller">
     <div class="seller-content">
       <div class="overview">
         <h1 class="title">{{seller.name}}</h1>
@@ -29,24 +29,150 @@
           </li>
         </ul>
       </div>
+      <splite></splite>
+      <div class="bulletin">
+        <h1 class="title">公告与活动</h1>
+        <div class="content-wrapper border-1px">
+          <p class="content">{{seller.bulletin}}</p>
+        </div>
+        <ul v-if="seller.supports" class="supports">
+        <li class="support-item border-1px" v-for="(item,index) in seller.supports">
+          <span class="icon" :class="classMap[item.type]"></span>
+          <span class="text">{{item.description}}</span>
+        </li>
+      </ul>
+      </div>
     </div>
   </div>
 </template>
-
 <script type="text/ecmascript-6">
 import star from '../../components/star/star';
+import splite from '../../components/splite/splite';
+import BScroll from 'better-scroll';
+const ERR_OK= 0;
 export default {
   props: {
     seller: {
       type: Object
     }
   },
+   created () {
+    this.classMap = ['decrease', 'discount', 'guarantee', 'invoice', 'special'];
+  },
+  ready(){
+     this.$http.get('/api/seller').then((response) => {
+      response = response.body;
+      if (response.errno === ERR_OK) {
+        this.seller = response.data;
+        this.$nextTick(() => {
+          this.scroll = new BScroll(this.$refs.seller,{
+            click: true
+          });
+        });
+      }
+    });
+  },
   components: {
     star,
+    splite,
   },
 
   };
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+@import "../../common/stylus/mixin.styl"
+  .seller
+    position: absolute
+    top: 174px
+    bottom: 0
+    left: 0
+    width: 100%
+    overflow: hidden
+    .overview
+      padding: 18px
+      .title
+        margin-bottom: 8px
+        line-height: 14px
+        color: rgb(7,17,27)
+        font-size: 14px
+      .desc
+        padding-bottom: 18px
+        border-1px(rba(7,17,27,0.1))
+        font-size: 0
+        .star
+          display: inline-block
+          margin-right: 8px
+          vertical-align: top
+        .text
+          display: inline-block
+          line-height: 18px
+          margin-right: 12px
+          vertical-align: top
+          font-size: 10px
+          color: rgb(77,85,93)
+      .remark
+        display: flex
+        padidng-top: 18px
+        .block
+          flex: 1
+          text-align: center
+          border-right: 1px solid rgba(7,17,27,0.1)
+          &:last-child
+            border: none
+          h2
+            margin-bottom: 4px
+            line-height: 10px
+            font-size: 10px
+            color: rgb(147,153,159)
+          .content
+            line-hieght: 24px
+            font-size: 10px
+            color: rgb(7,17,27)
+            .stress
+              font-size: 24px
+    .bulletin
+      padding: 18px 18px 0 18px
+      .title
+        margin-bottom: 8px
+        line-height: 14px
+        color: rgb(7,17,27)
+        font-size: 14px
+      .content-wrapper
+        padding: 0 12px 16px 12px
+        border-1px(rba(7,17,27,0.1))
+        .content
+          line-height: 24px
+          font-size: 12px
+          color: rgb(240,20,20)       
+      .supports
+        .support-item
+          padding: 16px 12px
+          border-1px(rgba(7,17,27,0.1))
+          font-size: 0
+          .icon
+            display: inline-block
+            width: 16px
+            height: 16px
+            vertical-align: top
+            margin-right: 6px
+            background-size: 16px 16px
+            background-repeat: no-repeat
+            &.decrease
+              bg-image('decrease_4')
+            &.discount
+              bg-image('discount_4')
+            &.guarantee
+              bg-image('guarantee_4')
+            &.invoice
+              bg-image('invoice_4')
+            &.special
+              bg-image('special_4')
+          .text
+            line-height: 16px
+            font-size: 12px
+            color: rgb(7,17,27)
+            
+          
+      
 </style>
